ae9fe9c688abd178e2de84413d66b527bcbae086
fahad-zakir <fahadzakir81@gmail.com>
2021-05-05 19:30:25 -0500

    added-new-div

--- src/FormSignup.js
+++ src/FormSignup.js
@@ -2,29 +2,46 @@ import React from 'react'
 
 const FormSignup = () => {
     return (
-   <div className="form-content-right">
-       <form className="form">
+   <div className='form-content-right'>
+       <form className='form'>
            <h1>Get started with us today! Create your account by filling out your information below</h1>
        </form>
-       <div className="form-inputs">
-           <label htmlFor="username" 
-           className="form-label">
+       <div className='form-inputs'>
+           <label htmlFor='username' 
+           className='form-label'>
             Username
             </label>
-            <input type="text" 
+            <input 
+            type='text' 
+            id='username'
             name='username'
-            className="form-input"
+            className='form-input'
             placeholder='Enter your username'
             />
        </div>
-        <div className="form-inputs">
-           <label htmlFor="email" 
-           className="form-label">
+        <div className='form-inputs'>
+           <label htmlFor='password'
+           className='form-label'>
+            password
+            </label>
+            <input 
+            id='password'
+            type='password'
+            name='password'
+            className='form-input'
+            placeholder='Enter your password'
+            />
+       </div>
+        <div className='form-inputs'>
+           <label htmlFor='email'
+           className='form-label'>
             Email
             </label>
-            <input type="text" 
+            <input 
+            id='email'
+            type='email'
             name='email'
-            className="form-input"
+            className='form-input'
             placeholder='Enter your email'
             />
        </div>
